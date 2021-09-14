8b10ba3f10b72335b1fe694309169a9d505e218a
lucy74310 <lucy74310@gmail.com>
2021-09-14 13:39:13 +0900

    Register, Login, Auth

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const {User} = require('./models/User')
-
+const {auth} = require('./middleware/auth')
 // application/x-www-form-urlencoded 타입으로 된것을 가져올 수 있게 해주는 
 app.use(bodyParser.urlencoded({extended: true}))
 
 // application/json 타입 분석해서 가져올수 있게 
 app.use(bodyParser.json())
+
+// token 정보 쿠키에 저장하기 위해
+app.use(cookieParser())
+
 
 const config = require('./config/key')
 
@@ -39,7 +44,9 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err));
 
 
-app.post('/register', (req, res) => {
+
+/** 회원가입 **/
+app.post('/api/users/register', (req, res) => {
     //회원가입할때 필요한 정보들을 client 에서 가져오면 그것들을 db에 넣어준다.
     // {
     //     id: "hell",
@@ -58,5 +65,81 @@ app.post('/register', (req, res) => {
 
 
 
+/**로그인**/
+app.post('/api/users/login', (req, res) => {
+    // 1. DB안에서 요청된 EMAIL 찾기 
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        // 2. 요청된 EMAIL이 DB에 있다면, 비밀번호가 맞는 비밀번호 인지 확인
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+            return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 틀렸습니다."
+            })
+        })
+
+        // 3. 비밀번호도 맞다면, 토큰을 생성하기
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err)
+            
+            // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지, 세션 ... 어디가 안전한가.... 논란이 많음... 각기 장단점이 있다.
+            // 쿠키에 저장.  install cookie-parser --save
+            res.cookie("x_auth", user.token)
+            .status(200)
+            .json({
+                loginSuccess: true,
+                userId: user._id
+            })
+        })
+    })
+})
 
 
+/**인증(Auth)**/
+/**
+* auth 라는 middleware 사용. 
+* req를 받은 다음에 callback 하기전에 중간에서 뭘 해줌.
+
+role 1 어드민 2 특정부서어드민
+role 0 일반유저 role 0아님 관리자 
+ */
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 middleware를 통과해 왔다는 얘기는 authentication이 True라는 뜻 
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : ture,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+
+    })
+})
+
+
+
+console.log(process.env.NODE_ENV)
+app.get('/api/users/logout', auth, (req, res) => {
+    const user = new User(req.body)
+    user.token = null
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.clearCookie('x_auth').status(200).json({ success: true })
+    })
+
+    
+
+})
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+const config = require('../config/key')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -37,6 +40,85 @@ const userSchema = mongoose.Schema({
 //   },
   
 })
+
+//저장하기 전에
+userSchema.pre('save', function(next) {
+    //비밀번호를 암호화 시킨다.
+
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+    
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash // plain password를 hash 된 password로 변경 
+    
+                next()
+            })
+    
+        })
+    } else {
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword 1234
+    // hash $2b$10$TX.MfNVYZeOYppLVg2EI9urmDWW.Ckt9SaQuwg.3WHWvVWiuRZBta
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch) // isMatch는 이때 true
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // json web token 을 이용해서 token을 생성하기 
+
+    var token = jwt.sign(user._id.toHexString(), config.encodeKEY) //user._id + encodeKEY => token && 나중에 token에 encodeKEY을 넣으면 user._id가 나옴 
+
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+
+}
+
+
+
+/**
+ * Auth 기능 왜필요한가. 
+ * 로그인 되어 있는 유저만 이용가능 
+ * 관리자 유저만 이용가능
+ * 
+ * 1. Cookie에 저장된 token을 가져와서 복호화 -> user._id가 나옴
+ * 2. 
+ *  
+ */
+userSchema.methods.findByToken = function(token, cb) {
+    var user = this;
+
+    jwt.verify(token, config.encodeKEY, function(err, decoded) {
+        // 유저아이디를 이용해서 유저를 찾은 다음에 
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인 
+        user.findOne({
+            "_id": decoded,
+            "token": token
+        }, function(err, user) {
+            if (err) return cb(err)
+            cb(null, user)
+        })
+    })
+
+}
+
 
 
 const User = mongoose.model('User', userSchema)
