e06de57c33307e536f398998a9f43fcebca47262
lucy74310 <lucy74310@gmail.com>
2021-10-10 17:04:31 +0900

    list 작업 ing

--- server/routes/List.js
+++ server/routes/List.js
@@ -9,23 +9,22 @@ const { Check } = require("../models/Check");
 const { auth } = require("../middleware/auth");
 
 /**
- * Get All 체크리스트 
+ * Get All 체크리스트
  */
 router.get("/", auth, (req, res) => {
   res.send("hello this is lists api~");
 });
 
-
 /**
- * Create 체크리스트 
- * 
+ * Create 체크리스트
+ *
  * - Request Param
  * name
  * goal_count
- * 
+ *
  */
 
 router.post("/", auth, (req, res) => {
-  res.
-})
+  console.log(res.body);
+});
 module.exports = router;
